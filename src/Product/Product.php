3f7a79fcda4d86d07c9641401edfdf319f422015
quanpan <quanpan302@hotmail.com>
2018-08-20 13:12:17 +0200

    Update Product.php

--- src/Product/Product.php
+++ src/Product/Product.php
@@ -1,1 +1,26 @@
+<?php
+
+
+namespace Quanpan302\Wxrrd\Product;
+
+
+use Quanpan302\Pospal\Api;
+
+class Product extends Api
+{
+
+    const QUERY_PRODUCT_PAGES_API = '/pospal-api2/openapi/v1/productOpenApi/queryProductPages';
+
+    /**
+     * 分页查询全部商品信息
+     *
+     * @param array $params
+     * @return array
+     */
+    public function paginate($params = [])
+    {
+        return $this->request(self::QUERY_PRODUCT_PAGES_API, $params);
+    }
+
+}
 
