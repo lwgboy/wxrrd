576e1847f4c96815f8ce47f5f80abb018dbb6f14
quanpan <quanpan302@hotmail.com>
2018-08-20 13:13:06 +0200

    Update ServiceProvider.php

--- src/Ticket/ServiceProvider.php
+++ src/Ticket/ServiceProvider.php
@@ -1,1 +1,28 @@
+<?php
+
+
+namespace Quanpan302\Wxrrd\Ticket;
+
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     */
+    public function register(Container $pimple)
+    {
+        $pimple['ticket'] = function ($pimple) {
+            return new Ticket($pimple['config']->get('app_id'), $pimple['config']->get('app_key'), $pimple['config']->get('url'));
+        };
+    }
+}
 
