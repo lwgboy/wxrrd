470592fa52a527a0bbe7fc11bde617df91e25351
quanpan <quanpan302@hotmail.com>
2018-08-20 13:11:40 +0200

    Update ServiceProvider.php

--- src/Customer/ServiceProvider.php
+++ src/Customer/ServiceProvider.php
@@ -1,1 +1,28 @@
+<?php
+
+
+namespace Quanpan302\Wxrrd\Customer;
+
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     */
+    public function register(Container $pimple)
+    {
+        $pimple['customer'] = function ($pimple) {
+            return new Product($pimple['config']->get('app_id'), $pimple['config']->get('app_key'), $pimple['config']->get('url'));
+        };
+    }
+}
 
