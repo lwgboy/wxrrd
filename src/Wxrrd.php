4382c3a5bb6b0a5e95527eec77fb0e42d6086799
quanpan <quanpan302@hotmail.com>
2018-08-20 10:31:22 +0200

    Update Wxrrd.php

--- src/Wxrrd.php
+++ src/Wxrrd.php
@@ -1,1 +1,26 @@
+<?php
 
+
+namespace Quanpan302\Wxrrd;
+
+
+use Hanson\Foundation\Foundation;
+
+/**
+ * Class Pospal
+ * @package Hanson\Pospal
+ *
+ * @property \Hanson\Pospal\Ticket\Ticket $ticket
+ * @property \Hanson\Pospal\Customer\Customer $customer
+ * @property \Hanson\Pospal\Product\Product $product
+ */
+class Pospal extends Foundation
+{
+
+    protected $providers = [
+        Ticket\ServiceProvider::class,
+        Customer\ServiceProvider::class,
+        Product\ServiceProvider::class,
+    ];
+
+}
